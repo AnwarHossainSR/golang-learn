57e464808c1b9f012f46c2a9c28b36cd1a226037
Anwar Hossain <mahedianwar@gmail.com>
2024-11-02 22:43:10 +0600

    Short format of variablle

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    var name string = "John"
-    var age int = 30
-    var isStudent bool = false
+    name := "Alice"
+    age := 25
+    isStudent := true
 
     fmt.Println("Name:", name)
     fmt.Println("Age:", age)
